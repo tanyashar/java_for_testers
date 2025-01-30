feb7522f269f47469ac2457c232de29fb210d27d
tanya <tanya.sharipova@gmail.com>
2025-01-30 05:40:22 +0300

    Thread.sleep() + startTime, refactor MailHelper

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/MailHelper.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/MailHelper.java
@@ -4,20 +4,59 @@ import jakarta.mail.*;
 import ru.stqa.mantis.model.MailMessage;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
-import java.util.stream.Collectors;
 
 public class MailHelper extends HelperBase{
+
     public MailHelper(ApplicationManager manager) {
         super(manager);
     }
 
+    public List<MailMessage> receive(String username, String password, Duration duration) {
+        var start = System.currentTimeMillis(); // текущее время в милисекундах
+        while (System.currentTimeMillis() < start + duration.toMillis()) {
+            Store store = null;
+            try {
+                var inbox = getInbox(username, password);
+                inbox.open(Folder.READ_ONLY);
+                var messages = inbox.getMessages();
 
-    public List<MailMessage> receive(String username, String password) {
-        Store store = null;
+                var result = Arrays.stream(messages).map(m -> {
+                            try {
+                                return new MailMessage()
+                                        .withFrom(m.getFrom()[0].toString())
+                                        .withContent(m.getContent().toString());
+                            } catch (MessagingException | IOException e) { // catch нескольких типов исключений
+                                throw new RuntimeException(e);
+                            }
+                        })
+                        .toList();
+
+                inbox.close();
+                inbox.getStore().close();
+
+                if (result.size() > 0) {
+                    return result;
+                }
+            } catch (MessagingException e) {
+                throw new RuntimeException(e);
+            }
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        throw new RuntimeException("No mail");
+    }
+
+    private static Folder getInbox(String username, String password) {
         try {
+            Store store;
             // старт сессии
             var session = Session.getInstance(new Properties());
 
@@ -29,27 +68,32 @@ public class MailHelper extends HelperBase{
 
             // открыть почтовый ящик
             var inbox = store.getFolder("INBOX");
-            inbox.open(Folder.READ_ONLY);
-            var messages = inbox.getMessages();
-
-            var result = Arrays.stream(messages).map(m -> {
-                        try {
-                            return new MailMessage()
-                                    .withFrom(m.getFrom()[0].toString())
-                                    .withContent(m.getContent().toString());
-                        } catch (MessagingException | IOException e) { // catch нескольких типов исключений
-                            throw new RuntimeException(e);
-                        }
-                    })
-                    .toList();
-
-            inbox.close();
-            store.close();
-
-            return result;
-
+            return inbox;
         } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
     }
+
+    public void drain(String username, String password) {
+        // "осушить" = удалить все
+        try {
+        var inbox = getInbox(username, password);
+            inbox.open(Folder.READ_WRITE);
+
+            // пометить каждое письмо как удаленное
+            Arrays.stream(inbox.getMessages())
+                    .forEach(m -> {
+                        try {
+                            m.setFlag(Flags.Flag.DELETED, true);
+                        } catch (MessagingException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+            inbox.close();
+            inbox.getStore().close();
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- mantis_tests/src/test/java/ru/stqa/mantis/tests/MailTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/MailTests.java
@@ -3,13 +3,20 @@ package ru.stqa.mantis.tests;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 public class MailTests extends TestBase{
 
     @Test
     void canReceiveEmail() {
-        var messages = app.mail().receive("user1@localhost", "password");
+        var messages = app.mail().receive("user1@localhost", "password", Duration.ofSeconds(120));
 
         Assertions.assertEquals(1, messages.size());
         System.out.println(messages);
+    }
+
+    @Test
+    void canDrainInbox() {
+        app.mail().drain("user1@localhost", "password");
     }
 }
